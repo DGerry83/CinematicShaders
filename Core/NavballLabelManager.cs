44b6f493f96f10ed5660d56a7da26548685744b4
MrKreegland <gerrydwow@gmail.com>
2026-04-04 02:18:55 -0400

    radial icons swapped

--- Core/NavballLabelManager.cs
+++ Core/NavballLabelManager.cs
@@ -90,8 +90,8 @@ namespace CinematicShaders.Core
             "retrograde_sdf.png",
             "normal_sdf.png",
             "antinormal_sdf.png",
-            "radial_in_sdf.png",
             "radial_out_sdf.png",
+            "radial_in_sdf.png",
             "maneuver_sdf.png"
         };
         
@@ -101,8 +101,8 @@ namespace CinematicShaders.Core
             "retrograde_retro_sdf.png",
             "normal_retro_sdf.png",
             "antinormal_retro_sdf.png",
-            "radial_in_retro_sdf.png",
             "radial_out_retro_sdf.png",
+            "radial_in_retro_sdf.png",
             "maneuver_retro_sdf.png"
         };
         
