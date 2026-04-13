fab80026f97a87273686615d48ee1379b6158805
MrKreegland <gerrydwow@gmail.com>
2026-04-13 00:41:51 -0400

    Shifted button positions again

--- UI/UnifiedGridRegistry.cs
+++ UI/UnifiedGridRegistry.cs
@@ -173,8 +173,8 @@ namespace CinematicShaders.UI
             ["yes_button"] = new GridElementDefinition
             {
                 ElementId = "yes_button",
-                Position = GridPosition.At(3, 11),  // Aligned with CONFIRM_LAYER2_LINES: "   [YES]"
-                Width = 7,  // "[YES]" plus brackets
+                Position = GridPosition.At(3, 10),  // Row 10 (0-indexed) = 11th row, aligned with "   [YES]"
+                Width = 5,  // "[YES]" 
                 Height = 1,
                 Type = ElementType.Button,
                 Priority = 0
@@ -182,8 +182,8 @@ namespace CinematicShaders.UI
             ["no_button"] = new GridElementDefinition
             {
                 ElementId = "no_button",
-                Position = GridPosition.At(49, 11),  // Aligned with CONFIRM_LAYER2_LINES: "[NO]"
-                Width = 4,  // "[NO]" plus brackets
+                Position = GridPosition.At(52, 10),  // Row 10 (0-indexed) = 11th row, aligned with "[NO]"
+                Width = 4,  // "[NO]"
                 Height = 1,
                 Type = ElementType.Button,
                 Priority = 1
